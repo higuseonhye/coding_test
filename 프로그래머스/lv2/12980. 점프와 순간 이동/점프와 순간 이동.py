dc3b003b2fff62f4616e6e8b51cf0cce7d8ecebc
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-06 11:50:53 +0900

    Update 점프와 순간 이동.py

--- 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.py
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -8,6 +8,11 @@ def solution(n):
             count += 1
     return count
 
+"""
+This function implements a simple algorithm for counting the number of operations needed to make a number 0. The algorithm works as follows:
 
-
-
+1. Check if the number is divisible by 2. If it is, divide the number by 2.
+2. If the number is not divisible by 2, subtract 1 from the number and increment the count by 1.
+3. Repeat the process until the number becomes 0.
+4. Return the count.
+"""
