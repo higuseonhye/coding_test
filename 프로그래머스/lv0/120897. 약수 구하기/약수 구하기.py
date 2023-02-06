ed95a1379cf8c46d32c6c859cf10ac0785409903
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-06 11:10:56 +0900

    [level 0] Title: 약수 구하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.py
@@ -0,0 +1,11 @@
+"""
+def solution(n):
+    return [i for i in range(1, n+1) if n % i == 0]
+"""
+
+def solution(n):
+    result = []
+    for i in range(1, n+1):
+        if n % i == 0:
+            result.append(i)
+    return result
