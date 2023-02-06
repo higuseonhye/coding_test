07b0c056dc8e9ba335dbb4e6f7ade2aa1c61c02e
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-06 10:43:15 +0900

    Update 예상 대진표.py

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
@@ -13,6 +13,14 @@ def solution(n, a, b):
     return find_rank(n, a, b)
 
 """
-Using next to find the smallest i such that 2**i >= a and 2**i >= b is more efficient than using for loops and break statements. 
-And switching the order of a and b in the solution function before passing them to find_rank is a good idea to avoid redundant code.
+The find_rank function takes three arguments: n, a, and b. 
+It calculates the largest power of 2 that is less than or equal to a, represented by exp_a, 
+    and the largest power of 2 that is less than or equal to b, represented by exp_b. 
+If exp_a and exp_b are equal, the function recursively calls itself with updated arguments n-num, a-num, and b-num (where num is equal to 2^(exp_a-1)). 
+If exp_a is not equal to exp_b, the function returns exp_b.
+
+The solution function takes three arguments: n, a, and b. If a is greater than b, it swaps their values. 
+Then, it calls the find_rank function with n, a, and b as arguments and returns the result.
+
+The code returns the rank of the smallest power of 2 that is greater than or equal to the maximum of a and b within the first n positive integers.
 """
