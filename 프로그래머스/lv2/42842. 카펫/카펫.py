6a3d20c5c42fdaec89093ef39206a60b81d85ea7
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-06 09:19:20 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -0,0 +1,10 @@
+def solution(brown, yellow):
+    for i in range(1, int(yellow**0.5)+1):
+        if yellow % i == 0:
+            width = yellow // i + 2
+            height = i + 2
+            if width * height - yellow == brown:
+                return [width, height]
+
+
+
