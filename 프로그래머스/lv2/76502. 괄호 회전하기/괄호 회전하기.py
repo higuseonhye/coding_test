69c05cb2b81b08af44574401bea9a258b17fad23
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-06 12:57:37 +0900

    Update 괄호 회전하기.py

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -39,3 +39,30 @@ def solution(s):
             cnt += 1
 
     return cnt
+
+
+"""
+The given code defines a function called solution which takes a string s 
+    as input and returns the count of balanced sub-strings in the string.
+
+The code first checks if the length of the input string s is even. 
+If it is odd, then the function returns 0 because an odd-length string cannot contain balanced sub-strings.
+
+The code then creates a double-ended queue q from the input string s. 
+It then defines a set opens of opening brackets and a dictionary closes 
+    that maps closing brackets to their corresponding opening bracket.
+
+The code then defines an inner function is_valid_p which takes a double-ended queue q 
+    as input and returns True if the given queue represents a balanced string (using a stack-like approach).
+
+The code then checks if the first bracket in the q is an opening bracket and the queue represents a valid balanced string. 
+If it does not, the code rotates the queue to the left and repeats the same check until a valid balanced string is found. 
+If no valid balanced string is found, the function returns 0.
+
+Finally, the code iterates through the queue, updating a double-ended queue dq to maintain the current balanced status. 
+If the current bracket is a closing bracket that matches the last opening bracket in dq, 
+    the code pops the last opening bracket from dq. 
+Otherwise, the code adds the current bracket to dq. 
+If dq becomes empty, the code increments the count of balanced sub-strings. 
+The final result is returned by the function.
+"""
