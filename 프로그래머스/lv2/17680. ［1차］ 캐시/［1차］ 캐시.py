a0e2e745dd35ed208db046580285611a20c0b9ac
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-06 12:13:35 +0900

    Update ［1차］ 캐시.py

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -17,3 +17,23 @@ def solution(cacheSize, cities):
             cache[city] = None
     return answer
 
+
+"""
+This is a Python function that implements a LRU cache (Least Recently Used Cache). It takes two arguments:
+
+cacheSize: an integer representing the size of the cache
+cities: a list of strings, representing the cities that will be processed in the cache
+The function returns an integer representing the time taken to process all the cities in the list.
+
+The function works as follows:
+1. If the cache size is 0, the function immediately returns 5 * len(cities), meaning it takes 5 seconds to process each city.
+2. The function initializes a variable answer to store the total processing time 
+    and an ordered dictionary cache to store the processed cities.
+3. For each city in cities, it first converts it to upper case and checks if it's in the cache.
+4. If it is in the cache, the processing time is incremented by 1 second and the city 
+    is moved to the end of the ordered dictionary, indicating it was just used.
+5. If it is not in the cache, the processing time is incremented by 5 seconds and the ordered dictionary is checked for size. 
+    If the size is equal to the cache size, the least recently used item is popped from the ordered dictionary. 
+    The new city is then added to the cache.
+6. The function returns the total processing time, answer.
+"""
