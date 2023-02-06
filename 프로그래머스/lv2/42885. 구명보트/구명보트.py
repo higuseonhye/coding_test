daee345de0a3a2f7446641810af247ebc4b0cf71
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-06 09:46:00 +0900

    Update 구명보트.py

--- 프로그래머스/lv2/42885. 구명보트/구명보트.py
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -8,3 +8,9 @@ def solution(people, limit):
         j -= 1
         count += 1
     return count
+
+"""
+people 리스트를 오름차순으로 정렬 후, 가장 작은 값(people[i])와 가장 큰 값(people[j])을 검사하여 두 값의 합이 limit 이하이면 i를 1 증가시키고, 
+     j를 1 감소시켜 구명보트의 개수(count)를 1 증가시킵니다. 
+이 과정을 반복하여 모든 사람을 구출할 때까지 구명보트의 개수를 계산하여 return합니다.
+"""
