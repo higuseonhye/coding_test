9bfa4dbf201cf18249b16a330abcbca86636f352
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-06 12:03:33 +0900

    Update H－Index.py

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -4,3 +4,10 @@ def solution(citations):
         if citations[i] <= i:
             return i
     return len(citations)
+
+"""
+This code sorts the given citations array in descending order, then iterates over it. 
+For each iteration, it checks if the current value is less than or equal to the current index. 
+If this condition is satisfied, it returns the current index. 
+If the iteration completes, it returns the length of the citations array.
+"""
