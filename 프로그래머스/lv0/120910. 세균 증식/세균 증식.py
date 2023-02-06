3efa897019fea10fd4c0e6765b152a175797d551
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-06 10:59:29 +0900

    Update 세균 증식.py

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.py
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.py
@@ -1,2 +1,7 @@
 def solution(n: int, t: int) -> int:
     return n * (2 ** t)
+
+"""
+This function takes two parameters, n and t, both integers. It returns n * (2 ** t), 
+which calculates the number of bacteria after t hours if each bacteria doubles every hour.
+"""
