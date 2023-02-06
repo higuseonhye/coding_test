ce8cebdf79c2682b2ff02ec14a09e9b81b568668
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-06 09:59:26 +0900

    Update N개의 최소공배수.py

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -8,3 +8,16 @@ def solution(arr):
     for i in range(1, len(arr)):
         answer = lcm(answer, arr[i])
     return answer
+
+"""
+This code defines a function lcm which takes in two integers a and b and returns the least common multiple of the two numbers. 
+The least common multiple is calculated by multiplying a and b and then dividing the result by their greatest common divisor. 
+The greatest common divisor is calculated using the gcd function from the math library.
+
+The solution function takes in a list of integers arr and returns the least common multiple of all the numbers in the list. 
+It initializes a variable answer with the first element of the list, 
+    and then iterates over the remaining elements, updating the value of answer at each step 
+    to be the least common multiple of the current value of answer and the current element in the loop.
+    
+In the end, the final value of answer is returned as the solution.
+"""
