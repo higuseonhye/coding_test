00c796a3cc4095b84c2fc5e96098a1b31f5e4e68
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-07 13:50:03 +0900

    [level 3] Title: 정수 삼각형, Time: 33.66 ms, Memory: 14.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,6 +1,27 @@
+"""
 def solution(triangle):
     n = len(triangle)
-    for i in range(n-2, -1, -1):
-        for j in range(i+1):
-            triangle[i][j] += max(triangle[i+1][j], triangle[i+1][j+1])
-    return triangle[0][0]
+    dp = [[0] * (i + 1) for i in range(n)]
+    dp[0][0] = triangle[0][0]
+    
+    for i in range(1, n):
+        for j in range(i + 1):
+            if j == 0:
+                dp[i][j] = dp[i-1][j] + triangle[i][j]
+            elif j == i:
+                dp[i][j] = dp[i-1][j-1] + triangle[i][j]
+            else:
+                dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j]
+    return max(dp[n-1])
+"""
+
+def solution(triangle):
+    n = len(triangle)
+    dp = [0] * (n+1)
+    for i in range(1, n+1):
+        for j in range(i, 0, -1):
+            dp[j] = max(dp[j-1], dp[j]) + triangle[i-1][j-1]
+    return max(dp)
+
+
+
