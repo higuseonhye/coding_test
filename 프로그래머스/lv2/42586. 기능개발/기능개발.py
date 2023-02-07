c940ab6038768d9a984f14eba9bd3e200e6671af
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-07 13:25:16 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -1,3 +1,4 @@
+"""
 def solution(progresses, speeds):
     answer = []
     while progresses:
@@ -11,5 +12,22 @@ def solution(progresses, speeds):
         if cnt > 0:
             answer.append(cnt)
     return answer
+"""
+def solution(progresses, speeds):
+    answer = []
+    cnt = 0
+    while progresses:
+        for i in range(len(progresses)):
+            progresses[i] += speeds[i]
+        while progresses and progresses[0] >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            cnt += 1
+        if cnt > 0:
+            answer.append(cnt)
+            cnt = 0
+    return answer
+
+
 
 
