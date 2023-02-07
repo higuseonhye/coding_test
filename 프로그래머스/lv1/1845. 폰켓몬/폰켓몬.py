22ac769695417dd9e9ab75f04d48e63b10f3a62a
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-07 18:25:23 +0900

    [level 1] Title: 폰켓몬, Time: 1.16 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,2 +1,12 @@
+from collections import Counter
+
+def solution(nums):
+    count_dict = Counter(nums)
+    answer = len(nums)/2 if len(count_dict) > len(nums)/2 else len(count_dict) 
+
+    return answer
+
+"""
 def solution(ls):
     return min(len(ls)/2, len(set(ls)))
+"""
