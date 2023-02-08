dd1d460abc5d69dd0622bb63634f9237ff5b3aba
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-08 12:35:58 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -1,5 +1,40 @@
 def prime(n):
     if n <= 1: return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0: return False
+    return True
+
+def solution(n, k):
+    toDigit = ""
+    digit = 0
+    num = 1
+    while n >= num:
+        num *= k
+        digit += 1
+    for i in range(1, digit + 1):
+        digit_num = k ** (digit - i)
+        toDigit += str(n // digit_num)
+        n -= (n // digit_num) * digit_num
+
+    st = []
+    ans = 0
+    for i in range(len(toDigit)):
+        if toDigit[i] != '0':
+            st.append(toDigit[i])
+        else:
+            if len(st) != 0:
+                if prime(int(''.join(st))):
+                    ans += 1
+                st = []
+    if len(st) != 0:
+        if prime(int("".join(st))):
+            ans += 1
+    return ans
+
+
+"""
+def prime(n):
+    if n <= 1: return False
     i = 2
     while i*i <= n:
         if n%i == 0: return False
@@ -41,3 +76,4 @@ def solution(n, k):
             ans += 1
 
     return ans
+"""
