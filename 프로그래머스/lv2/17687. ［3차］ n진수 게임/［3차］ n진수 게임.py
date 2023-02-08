d1d74d2ecfd627f0efe6e1221b306646b4f7c94e
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-08 15:58:43 +0900

    [level 2] Title: [3차] n진수 게임, Time: 4.34 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
@@ -1,55 +1,24 @@
 def solution(n, t, m, p):
-    def change_binary(number):
+    alphabets = ['A', 'B', 'C', 'D', 'E', 'F']
+    
+    def change_base(num, base):
         result = ''
-        if number == 0 :
-            return str(0)
-        while number >= n:
-            result = str(number % n) + result
-            number //= n            
-        result = str(number % n) + result
+        while num >= base:
+            digit = num % base
+            result = (alphabets[digit - 10] if digit >= 10 else str(digit)) + result
+            num //= base
+        result = (alphabets[num - 10] if num >= 10 else str(num)) + result
         return result
-
-    def turn_alphabet(number):
-        alphabets = ['A', 'B', 'C', 'D', 'E', 'F']
-        if 10 <= number < n :
-            return alphabets[number % 10]        
-        if number >= 10:
-            number %= 10
-            return alphabets[number]
-        else :
-            return str(number)
-
-    def change_heximal(number):
-        result = ''
-        if number < n :
-            return turn_alphabet(number)
-        while number >= n:
-            result = turn_alphabet(number % n) + result
-            number //= n
-        result = turn_alphabet(number % n) + result
-        return result
-
+    
     cnt = 0
+    num_cnt = 0
     now_num = []
     result = ''
-    if n <= 10:
-        num_cnt = 0
-        while cnt < t :
-            cnt += 1
-            while len(now_num) < p :                
-                now_num.extend(list(change_binary(num_cnt)))
-                num_cnt += 1
-                if len(now_num) >= p:
-                    break
-            result += now_num[p - 1]
-            p += m
-    else :
-        num_cnt = 0
-        while cnt < t :
-            cnt += 1
-            while len(now_num) < p :
-                now_num.extend(list(change_heximal(num_cnt)))
-                num_cnt += 1
-            result += now_num[p - 1]
-            p += m
+    while cnt < t:
+        cnt += 1
+        while len(now_num) < p:
+            now_num.extend(list(change_base(num_cnt, n)))
+            num_cnt += 1
+        result += now_num[p - 1]
+        p += m
     return result
