7704e0df8a8da048c22d81c29754f4586aa3a636
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-08 14:08:45 +0900

    Update ［3차］ 압축.py

--- 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.py
+++ 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -13,3 +13,22 @@ def solution(msg):
 	if cur_str:
 		result.append(dic[cur_str])
 	return result
+
+"""
+주어진 코드는 문자열을 압축하는 프로그램입니다.
+
+dic은 26개의 문자(A-Z)에 대한 숫자(1-26) 매핑을 포함하는 딕셔너리입니다.
+cur_str은 현재 압축중인 문자열입니다.
+result는 압축된 결과를 저장할 리스트입니다.
+for c in text는 text의 각 문자에 대해 반복하는 루프입니다.
+- temp는 cur_str에 c를 추가한 것입니다.
+- if temp in dic은 temp가 dic에 존재하는지 검사합니다.
+- temp가 dic에 존재하면 cur_str을 temp로 업데이트합니다.
+- else는 temp가 dic에 존재하지 않는 경우입니다.
+- result에 dic[cur_str]을 추가합니다.
+- dic에 temp와 len(dic) + 1을 매핑하는 값을 추가합니다.
+- cur_str을 c로 업데이트합니다.
+if cur_str:은 cur_str이 비어 있지 않은 경우입니다.
+- result에 dic[cur_str]을 추가합니다.
+return result는 result를 반환합니다.
+"""
