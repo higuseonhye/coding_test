1f384d7962c02afe4cf45da90c7ae30ca3f59946
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-08 14:04:04 +0900

    [level 2] Title: [3차] 압축, Time: 0.13 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.py
+++ 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -1,14 +1,15 @@
 def solution(msg):
-    dic = {chr(65 + i): i + 1 for i in range(26)}
-    cur_str = ""
-    result = []
-    for c in msg:
-        temp = cur_str + c
-        if temp in dic:
-            cur_str = temp
-        else:
-            result.append(dic[cur_str])
-            dic[temp] = len(dic) + 1
-            cur_str = c
-    result.append(dic[cur_str])
-    return result
+	dic = {chr(65 + i): i + 1 for i in range(26)}
+	cur_str = ""
+	result = []
+	for c in msg:
+		temp = cur_str + c
+		if temp in dic:
+			cur_str = temp
+		else:
+			result.append(dic[cur_str])
+			dic[temp] = len(dic) + 1
+			cur_str = c
+	if cur_str:
+		result.append(dic[cur_str])
+	return result
