0d7cdd6e34c7be97e434347617f9ea9599f231e9
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-08 10:31:00 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 308.08 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,4 +1,3 @@
-"""
 def solution(elements):
     elementLen = len(elements)
     elements = elements * 2
@@ -9,21 +8,3 @@ def solution(elements):
             current_sum += elements[j]
             sums.add(current_sum)
     return len(sums)
-
-"""
-def solution(elements):
-    answer = 0
-    numberSet = set()
-    
-    elementLen = len(elements)
-    elements = elements * 2
-    
-    for i in range(elementLen) : 
-        for j in range(elementLen) : 
-            numberSet.add(sum(elements[j:j+i+1]))
-    answer = len(numberSet)
-    return answer
-
-
-
-
