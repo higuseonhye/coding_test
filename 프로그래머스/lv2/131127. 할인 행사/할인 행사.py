366660c37fd168397796cec81648f0478d94881c
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-09 14:52:52 +0900

    [level 2] Title: 할인 행사, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
@@ -1,33 +1,21 @@
 def solution(want, number, discount):
     ans = 0
-    wantList = dict()
+    wantList = dict(zip(want, number))
+    wantSet = set(want)
 
-    for i in range(len(want)):
-        wantList[want[i]] = number[i]
-    wantSet = set(wantList)
-
-    for i in range(10):
-        if discount[i] in wantList.keys():
-            wantList[discount[i]] -= 1
-            if wantList[discount[i]] == 0:
-                wantSet.remove(discount[i])
-    if len(wantSet) == 0:
-        ans+=1        
-    for j in range(10, len(discount)):
-
-        if discount[j] in wantList.keys():
+    for j in range(len(discount)):
+        if discount[j] in wantList:
             wantList[discount[j]] -= 1
             if wantList[discount[j]] == 0:
                 wantSet.remove(discount[j])
 
-
-        if discount[j-10] in wantList.keys():
-            wantList[discount[j-10]] += 1
-            if wantList[discount[j-10]] == 1:
-                wantSet.add(discount[j-10])
+        if j >= 10:
+            if discount[j-10] in wantList:
+                wantList[discount[j-10]] += 1
+                if wantList[discount[j-10]] == 1:
+                    wantSet.add(discount[j-10])
 
         if len(wantSet) == 0:
             ans+=1 
 
     return ans
-
