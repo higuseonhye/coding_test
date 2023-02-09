7f4b983317930f41633c1c7f62f2052e10fb8eee
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-09 15:13:25 +0900

    Update 할인 행사.py

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
@@ -19,3 +19,24 @@ def solution(want, number, discount):
             ans+=1 
 
     return ans
+
+"""
+이 Python 코드는 주어진 want, number, discount 리스트를 사용하여 할인 기회가 몇 번 있었는지 계산하는 프로그램입니다.
+
+ans 변수를 0으로 초기화하고, wantList 딕셔너리와 wantSet 집합을 생성합니다. 
+wantList 딕셔너리는 want 리스트와 number 리스트의 값을 매핑하여 생성되고, wantSet 집합은 want 리스트의 값을 저장합니다.
+
+discount 리스트에서 각 할인 기회에 대해 반복문을 실행합니다.
+
+현재 할인 기회에 해당하는 아이템이 wantList에 있는 경우, 해당 아이템의 수량을 1 감소시키고, 수량이 0이 되었을 경우 wantSet에서 해당 아이템을 제거합니다.
+
+현재 할인 기회의 인덱스가 10 이상인 경우, 10 개 전의 할인 기회에 해당하는 아이템을 wantList에서 검색하고, 수량을 1 증가시키고, 
+수량이 1이 되었을 경우 wantSet에 해당 아이템을 추가
+"""
+"""
+wantList와 wantSet을 초기화합니다. 
+    wantList는 want와 number 목록을 매핑하는 사전이고, wantSet은 wantList에서 남아 있는 물건의 목록을 나타냅니다.
+for 루프를 사용하여 discount 목록을 순회합니다.
+각 물건이 wantList에 있는지 확인합니다. 
+    있다면, 해당 물건에 대한 구입 요구 수를 1 감소시킵니다. 구입 요구 수가 0이 된다면, wantSet에서 해당 물건을 제거합니다.
+"""
