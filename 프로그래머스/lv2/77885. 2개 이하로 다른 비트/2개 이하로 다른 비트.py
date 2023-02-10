507ace9766e813bcafcc70f51c765c14afeb7197
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-10 15:34:09 +0900

    Update 2개 이하로 다른 비트.py

--- 프로그래머스/lv2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
+++ 프로그래머스/lv2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
@@ -1,4 +1,3 @@
-# 2진수 변환
 def dec_to_bin(n):
     if n == 0:
         return '0'
@@ -9,7 +8,6 @@ def dec_to_bin(n):
         
     return "1" + bin
 
-# 10진수 변환
 def bin_to_dec(bin):
     digit = 1
     dec = 0
@@ -43,3 +41,12 @@ def solution(numbers):
         answer.append(bin_to_dec(new_bin))
                           
     return answer
+
+"""
+이 코드는 solution 함수를 통해서 10진수 리스트를 받아 각 숫자들의 가장 작은 2진수로의 변환을 수행한 결과를 리스트 형태로 반환하는 코드입니다.
+
+- dec_to_bin 함수는 10진수 숫자를 2진수 문자열로 변환하는 함수이며, 
+- bin_to_dec 함수는 2진수 문자열을 10진수 숫자로 변환하는 함수입니다. 
+- solution 함수는 입력받은 10진수 리스트를 각각 2진수 문자열로 변환한 뒤, 
+    각 2진수 문자열의 마지막 비트가 0인 경우와 1인 경우에 따라서 새로운 2진수 문자열을 생성하고, 이를 10진수로 변환하여 반환합니다.
+"""
