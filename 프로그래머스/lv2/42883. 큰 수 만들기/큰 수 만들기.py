823535c58691cf4d3b8594380e18a9daf8dc17f5
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-11 01:14:10 +0900

    Update 큰 수 만들기.py

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -8,3 +8,26 @@ def solution(number, k):
     if k:
         stack = stack[:-k]
     return ''.join(stack)
+
+"""
+Overall, this code implements a greedy algorithm. 
+We loop through the digits of the input number and remove the smaller digits first, 
+    until we reach the desired length or we can no longer remove any more elements.
+The result is the largest number we can get.
+"""
+"""
+Let's go through the code line by line:
+
+stack = []: 
+    - We initialize an empty list stack to store the final number we want to get.
+for i in range(len(number)):: 
+    - We loop through the input number and get each digit.
+while k and stack and stack[-1] < number[i]:: 
+    - We keep popping the elements from the stack until we can no longer remove any elements or k is 0. The elements we pop are those that are smaller than the current digit, number[i]. This is because we want to get the largest possible number, so we want to remove the smaller digits first.
+stack.append(number[i]): 
+    - We append the current digit to the stack.
+if k:: 
+    - If k is still greater than 0, we remove the last k elements from the stack as they are the smallest elements.
+return ''.join(stack): 
+    - We return the stack as a string by joining its elements.
+"""
