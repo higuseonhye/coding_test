c501f257e48767421efd7726e55a31f6f8ca9ac9
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-10 16:24:02 +0900

    [level 3] Title: 베스트앨범, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
@@ -1,27 +1,14 @@
 def solution(genres, plays):
-    answer = []
-    d = {} # hash
-    dN = {} # for calculating genres rank
-
-    # assigning into dictionary
-    for i in range(len(genres)):
-        if genres[i] in d:
-            d[genres[i]].append((plays[i], i))
-            dN[genres[i]] += plays[i]
+    dic = {}
+    for i, genre in enumerate(genres):
+        if genre in dic:
+            dic[genre].append((i, plays[i]))
         else:
-            d[genres[i]] = [(plays[i], i)]
-            dN[genres[i]] = plays[i]
-
-    # sorting in descending order for genres rank popularity.
-    rank = sorted(dN, key=lambda x:dN[x], reverse=True)
-
-    # getting item(genres) in descending order.
-    for item in rank:
-        # sorting songs based on the number of plays in descending order.
-        temp = sorted(d[item], key=lambda x:(x[0],-x[1]), reverse=True)
-        # printing twice only.
-        for i in range(2):
-            answer.append(temp[i][1])
-            # if a particular genre has only one song, break the loop.
-            if len(d[item])<2: break
+            dic[genre] = [(i, plays[i])]
+    dic = {k: sorted(v, key=lambda x: x[1], reverse=True) for k, v in dic.items()}
+    dic = sorted(dic.items(), key=lambda x: sum(map(lambda y: y[1], x[1])), reverse=True)
+    answer = []
+    for _, lst in dic:
+        for i in range(min(len(lst), 2)):
+            answer.append(lst[i][0])
     return answer
