5e4cbb598e40d216c9df2b43953b63e96730119e
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-10 12:41:31 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 2.01 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -3,10 +3,8 @@ import re
 def solution(files):
     answer = []
     
-    head_num_tail = [re.split(r"([0-9]+)", file) for file in files]
-    
+    head_num_tail = [re.split(r"([0-9]+)", file) for file in files]    
     sorted_head_num_tail = sorted(head_num_tail, key=lambda x: (x[0].lower(), int(x[1])))
-    
     answer = ["".join(h_n_t) for h_n_t in sorted_head_num_tail]
     
     return answer
