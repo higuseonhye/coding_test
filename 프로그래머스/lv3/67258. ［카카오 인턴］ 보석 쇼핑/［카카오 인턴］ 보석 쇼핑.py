04118158117746e3b9febd129c8f6c6089caf637
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-11 10:45:45 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 129.36 ms, Memory: 41.1 MB -BaekjoonHub

--- 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -1,25 +1,20 @@
 def solution(gems):
-
-    TYPE_NUM = len(set(gems))
-    GEM_NUM = len(gems)
-    cur_shop = {gems[0]: 1}
-    cand = []
-    l_idx, r_idx = 0, 0
-    DIST, RESULT = 0, 1
-
-    while l_idx < GEM_NUM and r_idx < GEM_NUM:
-        if len(cur_shop) < TYPE_NUM:
-            r_idx += 1
-            if r_idx == GEM_NUM:
+    gem_types = len(set(gems))
+    gem_count = len(gems)
+    gem_dict = {gems[0]: 1}
+    solutions = []
+    left_index, right_index = 0, 0
+    while left_index < gem_count and right_index < gem_count:
+        if len(gem_dict) < gem_types:
+            right_index += 1
+            if right_index == gem_count:
                 break
-            cur_shop[gems[r_idx]] = cur_shop.get(gems[r_idx], 0) + 1
+            gem_dict[gems[right_index]] = gem_dict.get(gems[right_index], 0) + 1
         else:
-            cand.append((r_idx-l_idx, [l_idx+1, r_idx+1]))
-            cur_shop[gems[l_idx]] -= 1
-            if cur_shop[gems[l_idx]] == 0:
-                del cur_shop[gems[l_idx]]
-            l_idx += 1
-
-    cand = sorted(cand, key=lambda x: (x[DIST], x[RESULT]))
-
-    return cand[0][RESULT]
+            solutions.append((right_index-left_index + 1, [left_index+1, right_index+1]))
+            gem_dict[gems[left_index]] -= 1
+            if gem_dict[gems[left_index]] == 0:
+                del gem_dict[gems[left_index]]
+            left_index += 1
+    solutions.sort(key=lambda x: (x[0], x[1]))
+    return solutions[0][1]
