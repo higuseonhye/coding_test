7c74f3d278e82e3effecf9784fe4305fe54f6269
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-11 09:39:20 +0900

    Update 불량 사용자.py

--- 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.py
@@ -25,3 +25,56 @@ def solution(user_id, banned_id):
     used = []
     dfs(user_id, banned_id, 0, used, answer)
     return len(set(tuple(sorted(a)) for a in answer))
+
+"""
+This is a Python 3 code that implements a solution to a problem where given a list of user IDs and a list of banned IDs, 
+    the task is to find the number of ways to ban users, where a user is banned if their user ID matches any of the banned IDs.
+The code consists of two main functions: solution and dfs.
+
+The solution function starts by defining another function called match which takes two arguments, a user ID and a banned ID, 
+    and returns a Boolean indicating whether the user ID matches the banned ID. 
+This is done by checking the length of both IDs and whether each character in the user ID matches the corresponding character in the banned ID, with an exception for the '*' character in the banned ID, which can match any character in the user ID.
+
+The solution function then calls the dfs function to perform a depth-first search (DFS) through all possible combinations of banned user IDs. 
+The dfs function takes the following arguments: user_id, banned_id, banned_idx, used, and answer. 
+    - user_id and banned_id are the lists of all user IDs and banned IDs, respectively. 
+    - banned_idx is the current index of the banned ID that is being matched against the user IDs. 
+    - used is a list of indices of user IDs that have already been used. 
+    - answer is a list that will contain all possible combinations of banned user IDs that satisfy the matching criteria.
+
+The dfs function uses a for loop to iterate through all user IDs, and checks if a user ID has already been used (i.e. its index is in used). 
+If not, it calls the match function to check if the current user ID matches the current banned ID. 
+If the match is successful, the index of the current user ID is added to used, and the dfs function is called recursively with the next banned ID. 
+This continues until all banned IDs have been matched against user IDs, at which point a copy of used is added to answer.
+
+After the DFS is completed, the solution function returns the length of the set of sorted tuples of all possible combinations of banned user IDs stored in answer, 
+    to remove any duplicates. 
+This is because the same banned user IDs can be obtained in different order, and the set only keeps unique elements.
+
+In summary, this code implements a DFS algorithm to find all possible combinations of banned user IDs that match the banned IDs, 
+    and returns the number of unique combinations.
+"""
+"""
+1. The code defines a function solution that takes in two arguments user_id and banned_id.
+2. Within the solution function, another function match is defined. 
+    - The match function takes two arguments, user and ban. 
+    - It first checks if the length of user and ban are not equal, and if so, returns False.
+3. The for loop iterates through the tuples of user and ban created using the zip function. 
+    - For each iteration, the loop checks if b is equal to '*'. 
+    - If it is, the loop continues to the next iteration.
+4. If b is not equal to '*', the loop checks if u and b are equal. 
+    - If they are not equal, the function returns False.
+5. If the loop completes without returning False, the function returns True.
+6. Another function dfs is defined within the solution function. 
+    - It takes in five arguments user_id, banned_id, banned_idx, used, and answer.
+7. The function checks if banned_idx is equal to the length of banned_id. 
+    - If it is, the function appends a copy of the used list to the answer list and returns.
+8. The for loop iterates through the enumerated user_id list. 
+    - If the current index i is in the used list, the loop continues to the next iteration.
+9. If i is not in used, the function checks if match(user, banned_id[banned_idx]) is True. 
+    - If it is, the function appends i to the used list and calls the dfs function recursively, passing in user_id, banned_id, banned_idx + 1, used, and answer.
+10. After the recursive call, the function removes the last element from the used list.
+11. Outside of the dfs function, two variables answer and used are initialized to be empty lists.
+12. The dfs function is called with arguments user_id, banned_id, 0, used, and answer.
+13. The function returns the length of the set of sorted tuples of answer.
+"""
