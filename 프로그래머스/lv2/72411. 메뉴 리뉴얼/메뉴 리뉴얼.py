76279c89bbe0fbcfa3d84f3caa367d3dca0957c4
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-11 09:55:33 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 0.72 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -1,25 +1,26 @@
 from itertools import combinations
-from collections import Counter
 
 def solution(orders, course):
-
+    
+    menu_cnt = {}   #코스요리메뉴 : 시킨 사람 수 
+    for s in orders:
+        for n in course:
+            for i in combinations(sorted(s), n):
+                print(i)
+                menu_cnt[i] = menu_cnt.get(i, 0) + 1
+    
+    dict = { n: [2, []] for n in course }  # 키 : [시킨 사람 수, [메뉴]]
+    for key in list(menu_cnt.keys()):
+        if  dict[len(key)][0] == menu_cnt[key]:
+            dict[len(key)][1].append(''.join(key))
+        elif dict[len(key)][0] < menu_cnt[key]:
+            dict[len(key)].clear()
+            dict[len(key)] = [menu_cnt[key], [''.join(key)]]
+            
     answer = []
-    
-    for n in course:
-        menu = []
-        for s in orders:
-            menu += list(combinations(sorted(s), n))
-        menu_cnt_s = Counter(menu).most_common()
-        max_cnt = 2
-        for menu_cnt in menu_cnt_s:
-            if menu_cnt[1] > max_cnt:
-                answer.append(''.join(menu_cnt[0]))
-                max_cnt = menu_cnt[1]
-            elif menu_cnt[1] == max_cnt:
-                answer.append(''.join(menu_cnt[0]))
-            else:
-                break
-    
+    for val in dict.values():
+        if val[1]:
+            answer.extend(val[1])
     answer.sort()
-    
+
     return answer
