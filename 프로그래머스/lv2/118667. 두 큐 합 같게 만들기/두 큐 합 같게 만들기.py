6ac94427b835fb4e44d2831589831a2509066c5d
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-16 15:19:13 +0900

    Update 두 큐 합 같게 만들기.py

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -24,3 +24,33 @@ def solution(queue1: List[int], queue2: List[int]) -> int:
         count += 1
 
     return -1
+
+
+"""
+from collections import deque                                       # import deque from collections module
+from typing import List                                             # import List from typing module
+
+def find_min_moves(queue1: List[int], queue2: List[int]) -> int:
+    q = deque(queue1 + queue2)                                      # concatenate queue1 and queue2 and create a deque object
+    target = sum(q) // 2                                            # calculate the target sum
+
+    left, right = 0, len(queue1) - 1                                # set left pointer to 0 and right pointer to len(queue1) - 1
+    curr_sum = sum(queue1)                                          # initialize current sum to the sum of queue1
+    count = 0                                                       # initialize counter to 0
+
+    while left < len(q) and right < len(q):                         # continue loop until both pointers are less than length of q
+        if curr_sum == target:                                      # if the current sum equals the target sum
+            return count                                            # return the number of moves taken
+
+        elif curr_sum < target and right < len(q) - 1:              # if the current sum is less than target sum and the right pointer is less than len(q) - 1
+            right += 1                                              # move right pointer to the right
+            curr_sum += q[right]                                    # add the new element to the current sum
+
+        else:                                                       # otherwise
+            curr_sum -= q[left]                                     # subtract the left element from the current sum
+            left += 1                                               # move the left pointer to the right
+
+        count += 1                                                  # increment the counter
+
+    return -1                                                       # return -1 if no solution found
+"""
