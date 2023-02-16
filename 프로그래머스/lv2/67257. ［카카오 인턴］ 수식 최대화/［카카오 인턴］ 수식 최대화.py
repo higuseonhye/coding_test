c79a3cdd52baff4b3280530c2c8cb8c4a05e6b47
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-16 15:08:00 +0900

    Update ［카카오 인턴］ 수식 최대화.py

--- 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
+++ 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
@@ -37,3 +37,23 @@ def solution(expression):
         # The final value of the expression is the only remaining element in the list of tokens
         max_value = max(max_value, abs(temp_tokens[0]))
     return max_value
+
+"""
+This code uses regular expressions and nested loops to find the maximum possible value of an arithmetic expression containing only +, -, and * operators.
+
+Here's a step-by-step explanation of the code:
+1. The re.findall method is used to create a list of numbers and operators from the expression.
+2. The list of possible operators is defined, and all possible combinations of operators are defined in the priority list.
+3. The calculate function is defined to calculate the value of an expression using a given operator.
+4. A variable max_value is initialized to 0 to keep track of the maximum possible value.
+5. The code iterates over all possible operator combinations defined in the priority list.
+6. A copy of the list of tokens and operators is created for each iteration.
+7. The code iterates over the current operator combination and performs calculations on the list of tokens.
+8. The final value of the expression is the only remaining element in the list of tokens. 
+   The absolute value of this value is calculated and compared to the current max_value, and the larger value is assigned to max_value.
+9. The function returns the final value of max_value.
+
+In summary, the code uses regular expressions to extract the numbers and operators from the input expression, 
+    and then iterates over all possible combinations of operators to calculate the maximum possible value of the expression. 
+The calculate function is used to perform the arithmetic calculations.
+"""
