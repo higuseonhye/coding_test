cf161d07a706174c47b1c98579167c333fde088a
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-16 15:23:11 +0900

    Update 스티커 모으기（2）.py

--- 프로그래머스/lv3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
+++ 프로그래머스/lv3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
@@ -24,3 +24,18 @@ def solution(sticker):
     
     # Return the maximum amount we can get
     return max(dp1[n - 2], dp2[n - 1])
+
+"""
+The function solution takes a list of integers sticker as input and returns the maximum amount that can be obtained by selecting non-adjacent elements from sticker.
+
+The code uses dynamic programming to solve the problem. It first checks for edge cases where the length of sticker is 1 or 2.
+
+It then initializes two dynamic programming tables, dp1 and dp2, 
+    which will store the maximum amount that can be obtained if we choose or don't choose the first sticker, respectively.
+
+The code then iterates through sticker and fills in the values of dp1 and dp2 using the dynamic programming formula:
+- dp[i] = max(dp[i - 1], dp[i - 2] + sticker[i])
+- where i is the current index, dp is either dp1 or dp2 depending on whether we are choosing the first sticker or not, and sticker is the input list.
+
+Finally, the code returns the maximum amount that can be obtained by choosing or not choosing the first sticker, depending on which one yields the higher value.
+"""
