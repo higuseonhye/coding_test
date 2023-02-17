76b8bea1da274676ed3fde68b1f29f47cc1356b3
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-17 13:34:40 +0900

    Update 전력망을 둘로 나누기.py

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -33,3 +33,29 @@ def solution(n, wires):
         answer = min(answer, diff)
 
     return answer
+
+"""
+The solution function takes two arguments: an integer n (the number of nodes in the graph), and a list of wires representing the edges of the graph. 
+Each wire is a tuple of two integers u and v representing an edge between nodes u and v. 
+The function returns an integer representing the minimum absolute difference in the sizes of the two connected components of the graph after removing one wire.
+
+The function works by trying each possible wire to remove and computing the size of the two connected components of the graph that remain after removing the wire. 
+It then takes the absolute difference between the sizes of the two connected components and keeps track of the smallest such difference seen so far.
+
+To compute the sizes of the connected components, the function performs a depth-first search on each node in the graph. 
+It creates an adjacency list to represent the graph after removing the wire, 
+    and initializes a list of boolean flags to indicate whether each node has been visited during the search. 
+It then starts a search from each unvisited node, and keeps track of the number of nodes visited during the search. 
+Once a search is complete, the function adds the size of the connected component to a list of component sizes.
+
+After trying each possible wire to remove, the function returns the smallest absolute difference in component sizes seen.
+
+You can test the function with the sample input provided in the problem:
+
+n = 9
+wires = [[1,3],[2,3],[3,4],[4,5],[4,6],[4,7],[7,8],[7,9]]
+print(solution(n, wires))  # Output: 3
+
+The output should be 3, since the smallest absolute difference in component sizes is obtained by removing the wire between nodes 4 and 5, 
+    resulting in component sizes of 4 and 5.
+"""
