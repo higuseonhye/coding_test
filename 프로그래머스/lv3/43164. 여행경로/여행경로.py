4c446d13ec65e2e9182c4bd7cd3eceffde6b2e26
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-17 10:44:38 +0900

    Update 여행경로.py

--- 프로그래머스/lv3/43164. 여행경로/여행경로.py
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.py
@@ -32,3 +32,31 @@ def solution(tickets):
     backtrack("ICN")
 
     return route
+
+"""
+Problem: 
+- Given a list of flights that connect airports, find the route that visits all the airports exactly once, starting from the given start airport. 
+- Each flight is represented by a tuple of (departure airport, arrival airport), and the answer should be a list of airport codes (strings) representing the route. 
+- If there are multiple possible routes, return the lexicographically smallest one.
+
+Here's the Python 3 code to solve this problem using backtracking:
+
+Here's how the code works:
+1. First, we create a dictionary flights to store the flights departing from each airport. 
+- We iterate through the list of tickets and add each flight to the dictionary under its departure airport. 
+- If an airport does not have any flights departing from it, we initialize an empty list for that airport.
+
+2. Next, we sort the flights for each airport in lexicographic order.
+
+3. We use backtracking to find the route. 
+- We define a recursive function backtrack that takes the current airport as input. 
+- The function first checks if it has found a valid route (i.e., the length of the route is equal to the number of tickets plus one). 
+- If so, it returns True. If the current airport does not have any flights departing from it, it returns False. 
+- Otherwise, it iterates through the flights departing from the current airport in lexicographic order. 
+- For each flight, it removes the flight from the dictionary, adds the arrival airport to the route, and recursively calls backtrack with the arrival airport as input. If backtrack returns True, it means that a valid route has been found, so the function returns True. Otherwise, it adds the flight back to the dictionary, removes the arrival airport from the route, and tries the next flight. If all flights have been tried and no valid route has been found, the function returns False.
+
+4. Finally, we start the backtracking from the given start airport ("ICN") and return the route.
+
+Note that this code assumes that the input is valid (e.g., there are no duplicate flights or disconnected airports), 
+so you may want to add some error checking if necessary.
+"""
