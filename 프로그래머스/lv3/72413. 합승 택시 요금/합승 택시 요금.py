c84c7059a6590113e2b30898c569fe0e6c810998
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-17 13:37:23 +0900

    Update 합승 택시 요금.py

--- 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -34,3 +34,30 @@ def solution(n, s, a, b, fares):
         answer = min(answer, fare)
 
     return answer
+
+"""
+The solution function takes five arguments: 
+    an integer n (the number of nodes in the graph), 
+    three integers s, a, and b (the starting node, the destination node for passenger A, and the destination node for passenger B, respectively), 
+    and a list of fares representing the edges of the graph. 
+Each fare is a tuple of three integers u, v, and w representing an edge between nodes u and v with weight w. 
+The function returns an integer representing the minimum fare that must be paid for both passengers to reach their destinations, 
+    assuming that they travel optimally and independently.
+
+The function works by first creating an adjacency list to represent the graph, 
+    and then computing the shortest distances between all pairs of nodes in the graph using Dijkstra's algorithm. 
+It then initializes the answer with the direct fares from s to a and b, and tries each possible transfer node t to see if a better fare can be found. 
+The total fare for each transfer node is computed as the sum of the fares from s to t, from t to a, and from t to b. 
+The function returns the minimum fare seen.
+
+You can test the function with the sample input provided in the problem:
+
+n = 6
+s = 4
+a = 6
+b = 2
+fares = [[4, 1, 10], [3, 5, 24], [5, 6, 2], [3, 1, 41], [5, 1, 24], [4, 6, 50], [2, 4, 66], [2, 3, 22], [1, 6, 25]]
+
+print(solution(n, s, a, b, fares))  # Output: 82
+The output should be 82, since the optimal transfer node is node 5, resulting in 82.
+"""
