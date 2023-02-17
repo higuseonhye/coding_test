18a547b2cc145b1c88cd055fef4c03858a707dd2
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-17 13:44:53 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/62048. 멀쩡한 사각형/멀쩡한 사각형.py
+++ 프로그래머스/lv2/62048. 멀쩡한 사각형/멀쩡한 사각형.py
@@ -1,4 +1,4 @@
-def solution(w, h):
+def solution(w: int, h: int) -> int:
     """
     Calculates the number of square tiles that can fit in a rectangular area with dimensions w and h,
     minus the number of tiles on the boundary.
@@ -8,12 +8,12 @@ def solution(w, h):
     :return: the number of square tiles that can fit in the rectangular area, minus the number of tiles on the boundary
     """
     
-    # Calculate the greatest common divisor of w and h
-    s = lambda a, b: a if not b else s(b, a % b)
-    gcd = s(w, h)
+    # Calculate the greatest common divisor of w and h using math.gcd
+    from math import gcd
+    gcd_w_h = gcd(w, h)
     
     # Calculate the number of tiles on the boundary
-    boundary_tiles = (w + h - gcd)
+    boundary_tiles = (w + h - gcd_w_h)
     
     # Calculate the total number of tiles
     total_tiles = w * h
