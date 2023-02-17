cfdb8164aaefc5cf499103682e4b77c25e89d7c8
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-17 17:06:02 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.11 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.py
+++ 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -3,34 +3,32 @@ from typing import List
 
 ROWS, COLS = 5, 5
 
-#     상   하  좌  우
-DC = [ 0, 0, -1, 1]
-DR = [-1, 1,  0, 0]
+DC = [0, 0, -1, 1]  # directions for columns
+DR = [-1, 1, 0, 0]  # directions for rows
 
 
-def solution(places):
-    global visited
-    
+def solution(places: List[str]) -> List[int]:
     ans = []
     for place in places:
         candidates = get_candidates(place)
         is_distancing = True
         for candidate in candidates:
-            visited = [[False] * COLS for _ in range(ROWS)]
-            if not dfs(place, candidate):
+            if not is_distanced(place, candidate):
                 is_distancing = False
                 break
-                
         ans.append(int(is_distancing))
     return ans
 
 
-def dfs(place:list, candidate:tuple, depth:int=0) -> bool:
-    # depth == 현재 탐색 중인 거리값(Current distance)
+def is_distanced(place: str, candidate: tuple, depth: int = 0, visited: List[List[bool]] = None) -> bool:
     if depth > 2:
         return True
     
+    if visited is None:
+        visited = [[False] * COLS for _ in range(ROWS)]
+    
     r_idx, c_idx = candidate
+    
     if r_idx < 0 or r_idx >= ROWS or c_idx < 0 or c_idx >= COLS:  # Ignore: Out of index
         return True
     if visited[r_idx][c_idx]:  # Ignore: Already visited node
@@ -38,20 +36,20 @@ def dfs(place:list, candidate:tuple, depth:int=0) -> bool:
     if place[r_idx][c_idx] == 'X':  # Ignore: 'X'(파티션)
         return True
     
-    # 거리값 2 이내의 응시자 존재 --> Fail
+    # There is a person within 2 seats, thus fail
     if depth != 0 and place[r_idx][c_idx] == 'P':
         return False
             
     visited[r_idx][c_idx] = True
-    for d_idx in range(4):  # 상, 하, 좌, 우
+    for d_idx in range(4):  # directions: up, down, left, right
         r = r_idx + DR[d_idx]
         c = c_idx + DC[d_idx]
-        if not dfs(place, (r, c), depth=depth+1):
+        if not is_distanced(place, (r, c), depth=depth+1, visited=visited):
             return False
     return True
 
 
-def get_candidates(place:list) -> List[tuple]:
+def get_candidates(place: str) -> List[tuple]:
     candidates = []
     for r_idx in range(ROWS):
         for c_idx in range(COLS):
