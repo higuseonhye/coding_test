58e63cd0e256389528bf1427ad4a5fe44907b931
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-17 10:18:36 +0900

    Update 배달.py

--- 프로그래머스/lv2/12978. 배달/배달.py
+++ 프로그래머스/lv2/12978. 배달/배달.py
@@ -29,3 +29,23 @@ def solution(N, road, K):
             answer += 1
 
     return answer
+
+"""
+Problem: 
+- There are N cities numbered from 1 to N, and M bidirectional roads connecting them. 
+- Each road has a travel cost. Starting from city 1, you want to visit all cities with the minimum cost possible. 
+- Return the minimum cost.
+
+Here's the Python 3 code to solve this problem using Dijkstra's algorithm:
+Here's how the code works:
+1. First, we create an adjacency list to represent the graph. 
+- For each road, we add both directions to the adjacency list.
+2. Next, we use Dijkstra's algorithm to find the shortest path from city 1 to all other cities. 
+- We initialize the distance to all cities to infinity, except for city 1, which we set to 0. 
+- We use a priority queue to keep track of the nodes we need to visit next, with the priority being the distance from city 1.
+3. After we have found the shortest distance to each city, we iterate through all cities and count the number of cities that can be visited within K distance.
+4. Finally, we return the answer.
+
+Note that this code assumes that the input is valid (e.g., there are no negative costs or cycles in the graph), 
+so you may want to add some error checking if necessary.
+"""
