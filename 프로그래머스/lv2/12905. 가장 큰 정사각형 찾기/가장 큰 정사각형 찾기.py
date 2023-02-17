ca83d91c074e5b4eae3f8bd42e04037e13573262
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-17 10:48:05 +0900

    Update 가장 큰 정사각형 찾기.py

--- 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.py
+++ 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.py
@@ -8,3 +8,26 @@ def solution(board):
                 dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
                 max_size = max(max_size, dp[i][j])
     return max_size ** 2
+
+"""
+Problem: Given a 2D matrix of 0's and 1's, find the size of the largest square of 1's in the matrix.
+
+Here's a Python 3 code to solve this problem using dynamic programming:
+
+Here's how the code works:
+
+1. We initialize a 2D array dp with all zeros. dp[i][j] will represent the size of the largest square of 1's that ends at position (i, j) in the matrix.
+
+2. We iterate over the positions in the matrix and update the values of dp. 
+- For each position (i, j), if the value of the corresponding element in the input board is 1, 
+  we set dp[i][j] to the minimum of the values of dp[i-1][j-1], dp[i-1][j], and dp[i][j-1], and add 1 to it. 
+- This is because the size of the largest square of 1's that ends at (i, j) is determined by the size of the largest square of 1's that ends at (i-1, j-1), (i-1, j), 
+  and (i, j-1). We take the minimum of these values to ensure that all sides of the square are valid.
+
+3. For each update of dp, we also update a variable max_size to keep track of the largest square of 1's seen so far.
+
+4. Finally, we return max_size squared, since we are asked to return the area of the largest square, not the size of its side.
+
+Note that this code assumes that the input is valid (e.g., the matrix is non-empty and all rows have the same length), 
+so you may want to add some error checking if necessary.
+"""
