309ba3bd2f3eaf12b2400471bcb893cd086d81f7
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-18 09:54:12 +0900

    Update 입국심사.py

--- 프로그래머스/lv3/43238. 입국심사/입국심사.py
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.py
@@ -26,3 +26,24 @@ def solution(n, times):
     
     # Return the minimum time required to process all applicants
     return left
+
+"""
+The solution function takes two arguments:
+
+n : an integer representing the number of applicants
+times : a list of integers representing the processing time for each immigration officer
+The function first sorts the times list in ascending order. 
+Then, it initializes the minimum and maximum time it can take to process all applicants. 
+The minimum time is 1 because it takes at least 1 minute to process an applicant. 
+The maximum time is n * times[-1] because the slowest immigration officer can process all applicants in n * times[-1] time.
+
+The function then performs a binary search to find the minimum time required to process all applicants. In each iteration, 
+    it calculates the mid time and counts the number of applicants that can be processed with the mid time. 
+If the number of processed applicants is greater than or equal to n, 
+    it means that we can process all applicants with the given mid time or less. 
+So, we update the right bound to mid-1. 
+Otherwise, the given time is not enough to process all applicants. 
+So, we update the left bound to mid+1.
+
+Finally, the function returns the minimum time required to process all applicants.
+"""
