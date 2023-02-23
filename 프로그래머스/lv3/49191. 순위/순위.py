0ab0df9a12ae9439aea8883200e82fff03d0418e
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-23 21:51:25 +0900

    [level 3] Title: 순위, Time: 5.39 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/lv3/49191. 순위/순위.py
+++ 프로그래머스/lv3/49191. 순위/순위.py
@@ -1,22 +1,23 @@
+from collections import defaultdict
+
 def solution(n, results):
-    win, lose = {}, {}
-    
-    for i in range(1, n+1):
-        win[i], lose[i] = set(), set()
-
+    # 경기 결과 그래프 생성
+    wins, loses = defaultdict(set), defaultdict(set)
     for i, j in results:
-        win[i].add(j)
-        lose[j].add(i)
+        wins[i].add(j)
+        loses[j].add(i)
 
-    for i in range(1, n+1):
-        for j in win[i]:
-            lose[j].update(lose[i])
-        for j in lose[i]:
-            win[j].update(win[i])
+    # 선수별 승리 가능한 선수 집합 찾기
+    for i in range(1, n + 1):
+        for loser in wins[i]:
+            loses[loser].update(loses[i])
+        for winner in loses[i]:
+            wins[winner].update(wins[i])
 
-    answer = 0
-    for i in range(1, n+1):
-        if len(win[i]) + len(lose[i]) == n-1:
-            answer += 1
-            
-    return answer
+    # 정확한 순위를 알 수 있는 선수 카운트
+    count = 0
+    for i in range(1, n + 1):
+        if len(wins[i]) + len(loses[i]) == n - 1:
+            count += 1
+
+    return count
