57a670d3b56694ba9e8c5298aa2deb7c50cef334
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-24 11:22:52 +0900

    Update ［카카오 인턴］ 경주로 건설.py

--- 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -35,3 +35,23 @@ def solution(board):
                 q.append((nx, ny, i))
     
     return min(cost[n-1][n-1])
+
+"""
+1. Import the deque class from the collections module.
+- Define the solution function that takes a board parameter.
+- Get the size of the board and set the infinity constant.
+- Create a 3D array of cost with dimensions (n, n, 4) to store the minimum cost to reach each point with each direction.
+- Define the dx and dy arrays to represent the change in x and y coordinates for each of the four directions (right, down, left, up).
+
+2. Initialize the minimum cost for each direction at the starting point (0, 0).
+- Create an empty deque object q and add the starting point with an invalid direction -1 to it.
+
+3. While q is not empty, pop the leftmost element and assign its x-coordinate, y-coordinate, and direction to x, y, and d.
+- For each of the four directions, calculate the new coordinates nx and ny.
+- If the new coordinates are outside the board or are blocked by a wall, skip to the next direction.
+- Calculate the new cost to reach the new point based on the previous direction.
+- If the new cost is smaller than the previous cost, update the minimum cost in the cost array and add the new point to q.
+- Repeat this process until q is empty.
+
+4. Return the minimum cost to reach the bottom-right corner (n-1, n-1) of the board.
+"""
