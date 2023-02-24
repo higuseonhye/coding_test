c61a146bf30f7d54abc1888fdd8e53703e180090
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-24 16:43:24 +0900

    Update 자물쇠와 열쇠.py

--- 프로그래머스/lv3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
+++ 프로그래머스/lv3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
@@ -37,3 +37,32 @@ def solution(key, lock):
                         new_lock[x+i][y+j] -= key[i][j]
     
     return False
+
+
+"""
+This code solves the problem of unlocking a lock with a given key in a 2D matrix.
+
+Here's a brief overview of what the code does:
+
+- rotate_a_matrix_by_90_degree(a): A helper function that takes a 2D matrix a and returns it rotated by 90 degrees.
+
+- check(new_lock): A helper function that takes a 2D matrix new_lock and checks if the central portion of size (len(new_lock) // 3) x (len(new_lock) // 3) is all 1's.
+
+- solution(key, lock): The main function that takes a 2D matrix key and a 2D matrix lock, and returns a boolean indicating whether the lock can be unlocked by the key.
+
+1. Initializes some variables.
+
+2. Copies the lock matrix into a larger matrix new_lock of size 3*n x 3*n with the central portion filled with the lock's values.
+
+3. Loops through each of the 4 rotations of the key.
+
+4. Loops through each position of the key in the new_lock matrix.
+
+5. Adds the key to the corresponding portion of new_lock.
+
+6. Checks if the central portion of new_lock is all 1's. If so, returns True.
+
+7. Subtracts the key from the corresponding portion of new_lock.
+
+8. Returns False if none of the rotations and positions of the key were able to unlock the lock.
+"""
