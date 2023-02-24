89d04d5d42862c7eb3fc5f59ef8324eb23fa0cd6
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-24 14:15:19 +0900

    [unrated] Title: 점 찍기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/140107. 점 찍기/점 찍기.py
+++ 프로그래머스/unrated/140107. 점 찍기/점 찍기.py
@@ -1,6 +1,6 @@
 def solution(k, d):
-    answer, d_square = 0, d ** 2
+    answer = 0
     for i in range(0, d + 1, k):
-        answer += int((d_square - i ** 2) ** .5) // k + 1
-    return answer
+        answer += (d ** 2 - i ** 2) ** 0.5 // k + 1
+    return int(answer)
 
