e083a24c12f74b5d19be176ee017b554d9084e2d
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-24 11:30:28 +0900

    Update 문자열 압축.py

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -31,3 +31,23 @@ def solution(s):
         min_len = min(min_len, len(compressed))
     
     return min_len
+
+"""
+Here's how the code works:
+
+Get the length of the input string s and initialize the min_len variable to n, which is the maximum possible length of the compressed string.
+
+For each possible length of substrings (from 1 to n), we compress the string using that length. 
+- We start by setting the start variable to 0, the count variable to 1, and the compressed variable to an empty string.
+
+We then iterate over the string in increments of the current length of substrings. 
+- For each substring of length length, we check if the next substring of length length is the same as the current substring. 
+- If they are the same, we increment the count variable. 
+- If they are different, we add the current substring to the compressed string along with the count (if the count is greater than 1) and reset the count variable to 1.
+
+After all substrings of length length have been processed, we add the remaining characters to the compressed string (if there are any).
+
+We update the min_len variable to be the minimum length of the compressed string found so far.
+
+After all possible lengths of substrings have been tried, we return min_len as the length of the shortest compressed string found.
+"""
