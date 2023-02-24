20ced5fb872c690ebfcdf03a22d67ddbf027e2ba
Seonhye Gu <34289848+higuseonhye@users.noreply.github.com>
2023-02-24 16:39:47 +0900

    Update 후보키.py

--- 프로그래머스/lv2/42890. 후보키/후보키.py
+++ 프로그래머스/lv2/42890. 후보키/후보키.py
@@ -20,3 +20,25 @@ def solution(relation):
             if set(unique[i]).issubset(set(unique[j])):
                 answer.discard(unique[j])
     return len(answer)
+
+"""
+Imports the combinations function from the itertools module.
+
+Defines a function solution that takes a single argument relation.
+
+- Initializes variables row_len and col_len with the number of rows and columns in the relation argument.
+
+- Creates a list candidates of all possible column combinations by iterating through each possible number of columns 
+    and generating all combinations of column indices using the combinations function.
+
+- Iterates through each combination of columns in candidates, 
+    then generates a list temp of tuples by selecting the values of the corresponding columns from each row in relation. 
+    
+  If temp contains unique tuples for each row in relation, the combination of columns in keys is added to a list unique of unique combinations.
+
+- Converts the list unique to a set answer and then iterates through each pair of combinations in unique. 
+
+  If the first combination is a subset of the second combination, the second combination is removed from answer. 
+  
+- Returns the length of answer, which is the number of unique combinations of columns that can be used as a key in a database table.
+"""
